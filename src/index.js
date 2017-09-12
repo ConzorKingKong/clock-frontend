c7df6a8f1a7f422e5499b588bff2de39d3efbc24
connor peshek <connor.peshek@kroger.com>
2017-09-12 16:09:26 -0400

    Remove unneeded dependencies.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,8 @@ import './index.styl'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Router, browserHistory} from 'react-router'
-import {createStore, applyMiddleware} from 'redux'
-import reducers from './reducers/index'
-import promise from 'redux-promise'
-import {Provider} from 'react-redux'
 import routes from './routes'
 
-const preStore = applyMiddleware(promise)(createStore)
-export const store = preStore(reducers)
-
 ReactDOM.render(
-  <Provider store={store}>
-    <Router routes={routes} history={browserHistory} />
-  </Provider>
+  <Router routes={routes} history={browserHistory} />
     , document.querySelector('.container'))
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {Route} from 'react-router'
 
-import App from './components/app/app'
-import FourZeroFour from './components/404/404'
+import App from './components/app'
+import FourZeroFour from './components/404'
 
 export default (
   <Route path='/' component={App}>
