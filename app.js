d207e82094b2db5a3eb11f8ab30794b1482c088e
Connor Peshek <connorpeshek@gmail.com>
2017-10-14 18:03:55 -0400

    Add redirect for heroku.

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.static('public'))
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    if (req.header['x-forwarded-proto'] !== 'https') res.redirect('https://conzorkingkongclock.herokuapp.com' + req.url)
     next()
 })
 
