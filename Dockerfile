f3973083cfc7b8e04fb2bce3d21b0f9b897d1167
Connor Peshek <connor@cloudflare.com>
2018-08-17 11:54:29 -0700

    Fix docker building process.

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,7 @@ ARG API_URL
 ENV API_URL=${API_URL}
 ADD src /app/src/
 RUN npm run build
-RUN rm -rf node_modules package.json package-lock.json webpack.config.js src
+RUN rm -rf node_modules webpack.config.js src
+RUN npm install --production
+RUN rm package-lock.json
 CMD ["npm", "start"]
--- app.js
+++ app.js
@@ -2,16 +2,6 @@ var express = require('express')
 var app = express()
 var path = require('path')
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      res.redirect(302, 'https://' + req.hostname + req.originalUrl)
-    } else {
-      next()
-    }
-  })
-}
-
 app.use(express.static('public'))
 
 app.use(function (req, res, next) {
